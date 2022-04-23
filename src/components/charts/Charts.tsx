e125bb400bc696d91666f84d205d30144f5fc3ce
Craig Kuriger <craigkuriger@Craig-2.local>
2022-04-23 15:31:15 +1200

    Chart axis formatting

--- src/components/charts/Charts.tsx
+++ src/components/charts/Charts.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  VictoryAxis,
   VictoryChart,
   VictoryClipContainer,
   VictoryLegend,
@@ -36,6 +37,16 @@ const Charts: React.FC = () => {
           <div className="col-md-6 text-center">
             <h4>Stock Price</h4>
             <VictoryChart>
+              <VictoryAxis
+                crossAxis={true}
+                domain={[250, 450]}
+                dependentAxis
+                tickFormat={(value) => `$${value}`}
+              />
+              <VictoryAxis
+                domain={[2017, 2022]}
+                tickFormat={(value) => value.toString()}
+              />
               <VictoryLine
                 groupComponent={
                   <VictoryClipContainer clipPadding={{ top: 5, right: 10 }} />
@@ -43,16 +54,22 @@ const Charts: React.FC = () => {
                 style={{
                   data: {
                     stroke: "#069f30",
-                    strokeWidth: 10,
+                    strokeWidth: 8,
                     strokeLinecap: "round",
                   },
                 }}
                 data={[
-                  { x: 18, y: 300.15 },
-                  { x: 19, y: 354.66 },
-                  { x: 20, y: 319.3 },
-                  { x: 21, y: 418.7 },
-                  { x: 22, y: 444.22, label: "Look!" },
+                  { x: 2017, y: 312.17 },
+                  { x: 2017.5, y: 309.15 },
+                  { x: 2018, y: 300.15 },
+                  { x: 2018.5, y: 320.77 },
+                  { x: 2019, y: 354.66 },
+                  { x: 2019.5, y: 364.56 },
+                  { x: 2020, y: 319.3 },
+                  { x: 2020.5, y: 329.3 },
+                  { x: 2021, y: 418.7 },
+                  { x: 2021.5, y: 428.5 },
+                  { x: 2022, y: 444.22 },
                 ]}
               />
             </VictoryChart>
